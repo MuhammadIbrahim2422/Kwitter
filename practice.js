c4a35bdd16a7634fdfcce738a6ed960393981e85
MuhammadIbrahim2422 <76980755+MuhammadIbrahim2422@users.noreply.github.com>
2021-08-16 19:25:44 -0500

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,14 @@
+
+//ADD YOUR FIREBASE LINKS
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBbGrXTOtfCY6qU5LKo6BUUVdbagryVzMA",
+    authDomain: "kwitter-96640.firebaseapp.com",
+    databaseURL: "https://kwitter-96640-default-rtdb.firebaseio.com",
+    projectId: "kwitter-96640",
+    storageBucket: "kwitter-96640.appspot.com",
+    messagingSenderId: "722065780462",
+    appId: "1:722065780462:web:e5c43464d9e01acb574dff"
+  };
+
+  firebase.initializeApp(firebaseConfig)
