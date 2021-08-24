fb152024f4b7f34a449adfb56f3a0f0f7c813e26
MuhammadIbrahim2422 <76980755+MuhammadIbrahim2422@users.noreply.github.com>
2021-08-23 19:39:18 -0500

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -5,3 +5,4 @@ function addUser() {
 
     window.location = "kwitter_room.html";
 }
+
--- practice.js
+++ practice.js
@@ -12,3 +12,13 @@ var firebaseConfig = {
   };
 
   firebase.initializeApp(firebaseConfig)
+
+
+  function addUser()
+  {
+      user_name = document.getElementById("user_name").nodeValue;
+      firebase.database().ref("/").child(user_name).update({
+          purpose : "adding user"
+      });
+  }
+  
